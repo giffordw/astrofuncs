8531b4c7008e9c06b74b9db694833262b7c89542
giffordw <dgifford19@gmail.com>
2014-08-01 13:04:56 -0400

    added setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='astrofuncs',
+      version='0.1',
+      description='A place to keep some of the functions I write in my daily work',
+      url='http://github.com/giffordw/astrofuncs',
+      author='Dan Gifford',
+      author_email='',
+      license='MIT',
+      packages=['findpotential'],
+      zip_safe=False)
+
